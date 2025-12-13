05c001fba54e4e25dfe393cd9ecda1ab8e36d1ee
Anna <timotityanna2006@gmail.com>
2025-12-13 14:36:05 +0100

    Első feltöltés

---
+++ menu.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="logo">
+        RideXspot
+        <div class="motto">
+            <p>Ott kezdődik az autód szíve, ahol a mi munkánk indul.</p>
+        </div>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php">Kezdőlap</a></li>
+            <li><a href="bemutatkozas.php">Bemutatkozás</a></li>
+            <li><a href="szolgaltatasok.php">Szolgáltatások</a></li>
+            <li><a href="megrendeles.php">Megrendelés</a></li>
+            <li><a href="autoberles.php">Autóbérlés</a></li>
+            <li><a href="szerviz.php">Szervíz</a></li>
+            <li><a href="kapcsolat.php">Kapcsolat</a></li>
+            <li><a href="bejelentkezes.php">Bejelentkezés</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ rolunk.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rólunk</title>
+    <link rel="stylesheet" href="rolunk.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+     <header class="site-header">
+    <nav class="nav">
+      <!-- Ide jöhet a nav (ha van) -->
+      <a href="index.html" class="logo">RideXspot ZRT.</a>
+    </nav>
+  </header>
+
+  <main class="about-section" role="main" aria-labelledby="rolunk-heading">
+    <div class="container">
+      <aside class="left">
+        <h1 id="rolunk-heading" class="huge-title">RÓLUNK</h1>
+
+        <div class="content">
+          <p class="lead">Üdvözöljük oldalunkon!</p>
+
+          <p>Ez az első jelentősebb projektünk, amelyen keresztül szeretnénk bemutatni szakmai elhivatottságunkat és értékrendünket. 
+
+Az autóipar iránti szenvedélyünk gyermekkorunk óta meghatározó része életünknek. Folyamatosan fejlesztjük tudásunkat és szakmai felkészültségünket, 
+jelenleg pedig a sikeres KRESZ-vizsga megszerzésén dolgozunk, hogy még magasabb szinten képviselhessük azt, amit szeretünk.
+            <strong class="motto">„Ha a motor szíve ide kerül, nálunk dobog tovább.”</strong>
+          </p>
+        </div>
+      </aside>
+
+      <figure class="right" aria-hidden="true">
+        <img src="./img/bmw_520_2022.jpg" alt="BMW" class="hero-car">
+      </figure>
+    </div>
+</main>
+
+
+</body>
+
+</html>
