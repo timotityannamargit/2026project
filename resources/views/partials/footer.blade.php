b8cc7d69695ee722b1716d0eb9cdfe4bce615594
Anna <timotityanna2006@gmail.com>
2026-02-20 16:16:23 +0100

    kesz az adatbaziséáá

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     
   </div>
   <div class="footer-bottom">
-    <p>&copy; 2025 FlottAutó. Minden jog fenntartva.</p>
+    <p>&copy; 2025 RideXspot. Minden jog fenntartva.</p>
     <span>📞 +36 30 123 4567</span>
     <span>⏰ H-P: 8:00 - 18:00</span>
     
