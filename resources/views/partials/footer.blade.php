d822cdcb9e1d849e2a93578d8c8104e0318c819e
Anna <timotityanna2006@gmail.com>
2026-02-22 21:15:26 +0100

    Kész az adatbázis

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,7 +5,6 @@
   <div class="footer-bottom">
     <p>&copy; 2025 RideXspot. Minden jog fenntartva.</p>
     <span>📞 +36 30 123 4567</span>
-    <span>⏰ H-P: 8:00 - 18:00</span>
-    
+    <span>⏰ H-P: 8:00 - 16:00</span>
   </div>
 </footer>
